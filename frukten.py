4c9928b4a1204656aaf55b2c74a17128518909db
Elias Sundström Hansen <elsu0317@storsjogymnasiet.se>
2023-03-14 10:35:55 +0100

    klart

--- frukten.py
+++ frukten.py
@@ -1,15 +1,38 @@
 import random
 
-Fruits = ("apple", "banana", "orange")
+Frukter = ("apple", "banana", "orange")
 looping = True
 
 def print_fruit(userinput):
     fnr = int(userinput)
-    print("\n" + Fruits[fnr-1] + "cums here \n")
+    print("\n" + Frukter[fnr-1] + "cums here \n")
 
 while looping: 
+
+    print("-------------------------------------------------")
+    print("\nfruitmachine\n")
+
+    i=1
+
+    for Frukt in Frukter:
+        print(str(i) + ": " + Frukt)
+    i+=1
+
+    Fruktnr = input ("vilken frukt")
+
+    print_fruit(Fruktnr)
+
+
+
     go = input("vill du välja en frukt till? j/n")
     print("\n")
 
     if (go == "n"):
         break
+
+    print ("------------------------------")
+    print("anothaone")
+    slumpfrukt = random.randint(1, 3)
+    print_fruit(slumpfrukt)
+
+
