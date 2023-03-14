a83c7b54a17d61c223aed2c9761de6f3894d19bb
Elias Sundström Hansen <elsu0317@storsjogymnasiet.se>
2023-03-14 09:29:18 +0100

    frukt random

---
+++ frukten.py
@@ -0,0 +1,11 @@
+import random
+
+Fruits = ("apple", "banana", "orange")
+looping = True
+
+while looping: 
+    go = input("vill du välja en frukt till? j/n")
+    print("\n")
+
+    if (go == "n"):
+        break
